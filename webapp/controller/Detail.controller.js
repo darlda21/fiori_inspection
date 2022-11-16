0841c54baa03594afd587ea0967582960ed31fd7
darlda21 <darl.da.gg@gmail.com>
2022-11-16 11:37:08 +0900

    fix: footer state 설정

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -9,6 +9,9 @@ sap.ui.define([
         onInit: function() {
 			var oOwnerComponent = this.getOwnerComponent();
 
+			this.oObjectPage = this.getView().byId("ObjectPageLayout");
+			this.bCurrentShowFooterState = this.oObjectPage.getShowFooter();
+
 			this.oRouter = oOwnerComponent.getRouter();
 			this.oModel = oOwnerComponent.getModel();
 
@@ -50,44 +53,49 @@ sap.ui.define([
 
 			if ( oList ){
 				var oBinding = oList.getBinding("rows");
-
-				if ( oBinding ){
-					oBinding.filter(oFilter);
-				}
+				if ( oBinding ){oBinding.filter(oFilter);}
 			}
 
 			if ( oList2 ){
 				var oBinding = oList2.getBinding("rows");
-
-				if ( oBinding ){
-					oBinding.filter(oFilter2);
-				}
+				if ( oBinding ){oBinding.filter(oFilter2);}
 			}
 
 			if ( oList3 ){
 				var oBinding = oList3.getBinding("items");
-
-				if ( oBinding ){
-					oBinding.filter(oFilter);
-				}
+				if ( oBinding ){oBinding.filter(oFilter);}
 			}
 
 			if ( oList4 ){
 				var oBinding = oList4.getBinding("items");
-
-				if ( oBinding ){
-					oBinding.filter(oFilter3);
-				}
+				if ( oBinding ){oBinding.filter(oFilter3);}
 			}
 			
 		},
 
 		onEditToggleButtonPress: function() {
+			this.oObjectPage.setShowFooter(!this.bCurrentShowFooterState);
+			this.bCurrentShowFooterState = !this.bCurrentShowFooterState;
+		},
 
-			var oObjectPage = this.getView().byId("ObjectPageLayout"),
-				bCurrentShowFooterState = oObjectPage.getShowFooter();
+		onPressAccept: function() {
+			var selectedIndices = this.getView().byId("suppliersTable").getSelectedIndices();
 
-			oObjectPage.setShowFooter(!bCurrentShowFooterState);
+			// 선택한 item이 있는지 확인
+			for(var i; i < selectedIndices.length; i++) {
+				// selectedIndicator에 해당하는 item path 저장
+				// item의 검수량이 구매량보다 작은지 확인
+				// 조건에 부합하다면 update
+
+			}
+
+			this.oObjectPage.setShowFooter(!this.bCurrentShowFooterState);
+			this.bCurrentShowFooterState = !this.bCurrentShowFooterState;
+		},
+
+		onPressReject: function() {
+			this.oObjectPage.setShowFooter(!this.bCurrentShowFooterState);
+			this.bCurrentShowFooterState = !this.bCurrentShowFooterState;
 		},
 
 		onExit: function () {
