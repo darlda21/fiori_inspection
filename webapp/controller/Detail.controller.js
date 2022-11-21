100cd67a8fba664fc28d5846edf81e88264e1cc8
darlda21 <darl.da.gg@gmail.com>
2022-11-21 09:58:16 +0900

    feat: 검수 및 데이터베이스 업데이트

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	'sap/ui/model/Filter',
-	'sap/ui/model/FilterOperator'
-], function (Controller, Filter, FilterOperator) {
+	'sap/ui/model/FilterOperator',
+	"sap/m/MessageBox",
+	"sap/m/MessageToast"
+], function (Controller, Filter, FilterOperator, MessageBox, MessageToast) {
 	"use strict";
 
 	return Controller.extend("syncbiginspection1.controller.Detail", {
@@ -79,14 +81,67 @@ sap.ui.define([
 		},
 
 		onPressAccept: function() {
-			var selectedIndices = this.getView().byId("suppliersTable").getSelectedIndices();
+			var oTable = this.getView().byId("suppliersTable");
+			var oTableRows = oTable.getRows();
+			var selectedIndices = oTable.getSelectedIndices();
+
+			var itemTable = [];		// 선택한 itemTable
+			var insValid = true;	// 검수가능 flag
+			var oIntyp;				// 검수타입 select 
+			var oIntme;				// 검수량 input
 
 			// 선택한 item이 있는지 확인
-			for(var i; i < selectedIndices.length; i++) {
-				// selectedIndicator에 해당하는 item path 저장
-				// item의 검수량이 구매량보다 작은지 확인
-				// 조건에 부합하다면 update
+			if(selectedIndices.length === 0) {
+				MessageBox.error("검수할 항목을 선택해주세요.");
+			} else {
+				for(var i = 0; i < selectedIndices.length; i++) {
+					var selectedRow = oTableRows[selectedIndices[i]];
+					var sRowPath = selectedRow.getBindingContext().sPath;
+					var oItem = selectedRow.getModel().getObject(sRowPath);	// 선택한 row의 item
 
+					oIntyp = selectedRow.getCells()[8].getSelectedKey();	// 검수타입 select 
+					oIntme = selectedRow.getCells()[9].getValue();			// 검수량 input
+
+					// 검수 가능한 조건인지 확인
+					if(oItem.Dstat !== 'A') { // 배송전 item이 있는지 확인
+						MessageBox.error("배송전인 항목은 검수가 불가능합니다.");
+						insValid = false;
+						break;
+					} else if(selectedRow.getCells()[9].getValueState() === 'Error') { // item의 검수량이 구매량보다 작은지 확인
+						MessageBox.error("검수량은 구매량보다 많을 수 없습니다.");
+						insValid = false;
+						break;
+					} else {
+						// 검수할 item을 itemTable에 저장
+						oItem.Intyp = oIntyp;
+						oItem.Intme = oIntme;
+
+						itemTable.push(oItem);
+					}
+				}
+
+				// 검수
+				if(insValid) {
+					// 선택한 라인의 데이터를 JSON 형태로 생성
+					// URL Encode 방식으로 인코딩 <-- 영어대소문자, 숫자를 제외한 한글, 특수문자등이 포함될 때만 추가
+					// Base64 방식으로 인코딩
+
+					let json = JSON.stringify(itemTable);
+					let uri = encodeURI(json);
+					let base64 = btoa(uri);
+					let oBody = {"Data" : base64};
+
+					debugger;
+					this.oModel.create("/jsonSet", oBody, {
+						success: function (oReturn) {
+							console.log("success",oReturn);
+							MessageToast.show("검수가 완료되었습니다.");
+						},
+						error: function (oError) {
+							console.log("error", oError);
+						}
+					})
+				}
 			}
 
 			this.oObjectPage.setShowFooter(!this.bCurrentShowFooterState);
